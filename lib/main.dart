761989537b27f58fed5dfbb22ac7ed6e27104c72
Mayank Bisht <mayankbisht17082004@gmail.com>
2024-11-03 18:17:50 +0530

    Login Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,30 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:waveconnect/responsive/mobile_screen_layout.dart';
 import 'package:waveconnect/responsive/responsive_layout_screen.dart';
 import 'package:waveconnect/responsive/web_screen_layout.dart';
+import 'package:waveconnect/screens/login_screen.dart';
 import 'package:waveconnect/utils/colors.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if(kIsWeb){
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(apiKey: "AIzaSyACEV7-CUuTyTVEsUBIwHMqBnHr--DHEKI", 
+      appId: "1:946834348663:web:c7b2f2ba04be8f5d3e4472", 
+      messagingSenderId:"946834348663", 
+      projectId: "waveconnect101",
+      storageBucket: "waveconnect101.firebasestorage.app",
+       ),
+    );
+  }
+  else{
+    await Firebase.initializeApp();
+  }
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -12,11 +34,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'WaveConnect',
-      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: mobileBackgroundColor,),
-      home:const ResponsiveLayout(
-        mobilescreenlayout: MobileScreenLayout(), 
-        webscreenlayout: WebScreenLayout(),
-        ),
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: mobileBackgroundColor,
+      ),
+      // home: const ResponsiveLayout(
+      //   mobilescreenlayout: MobileScreenLayout(),
+      //   webscreenlayout: WebScreenLayout(),
+      // ),
+
+      home: LoginScreen(),
     );
   }
 }
---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:waveconnect/utils/colors.dart';
+import 'package:waveconnect/widgets/text_field_input.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _emailcontroller = TextEditingController();
+  final TextEditingController _passwordcontroller = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    _emailcontroller.dispose();
+    _passwordcontroller.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 32),
+          width: double.infinity,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Flexible(child: Container(), flex: 2),
+              //svg image
+              SvgPicture.asset(
+                'assets/RZegbm01.svg',
+                color: primaryColor,
+                height: 64,
+              ),
+
+              const SizedBox(height: 64),
+              //text field input for email
+              TextFieldInput(
+                hintText: 'Enter your email',
+                textInputType: TextInputType.emailAddress,
+                textEditingController: _emailcontroller,
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              //text field input for password
+              TextFieldInput(
+                hintText: 'Enter your password',
+                textInputType: TextInputType.text,
+                textEditingController: _passwordcontroller,
+                isPass: true,
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              //login button
+              InkWell(
+                child: Container(
+                  child: const Text('Log in'),
+                  width: double.infinity,
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.symmetric(vertical: 12),
+                  decoration: const ShapeDecoration(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(4),
+                        ),
+                      ),
+                      color: blueColor),
+                ),
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              Flexible(child: Container(), flex: 2),
+              //Transitioning to sign up
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    child: const Text("Don't have an account?"),
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 8,
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(vertical: 8),
+                      child: const Text(
+                        "Sign up",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
